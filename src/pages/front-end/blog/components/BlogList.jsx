43c5e2b7333849e8af632421f2fb3dbd4806002f
shahidminhasgill <shahidminhas313@gmail.com>
2022-12-01 18:55:01 +0500

    blog listing page complete desktop screen

--- src/pages/front-end/blog/components/BlogList.jsx
+++ src/pages/front-end/blog/components/BlogList.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import blogimg from '../../../../assets/blog.png'
 import micImg from '../../../../assets/mic.png'
 import blog1 from '../../../../assets/blog1.png'
-import Ellipse from '../../../../assets/Ellipse.png'
+import recentpost1 from '../../../../assets/recentpost1.png'
+import arrowForwardIcon from '../../../../assets/arrow_forward_ios.svg'
 import CustomButton from '../../../../components/form/CustomButton'
 import { RxDoubleArrowDown } from 'react-icons/rx'
 import DateCard from '../../components/DateCard';
-import { settings } from '../../../../config/helper'
-import Slider from 'react-slick'
 import TopVanuesCard from '../../components/TopVanuesCard'
 import SimilarVanues from '../../components/SimilarVanues'
 
@@ -16,8 +14,8 @@ export default function BlogList() {
         <div className="container-fluid">
 
             <div className="row d-flex justify-content-around main-bloglist">
-                <div className="col-4">
-                    {/* <div className="blogHighlight"></div> */}
+                <div className="col-4 ">
+                    <div className="blogHighlight"></div>
                     <h2>SOMETHING <span>TO </span>DO </h2>
                     <hr />
                     <h3>BLOG NAME</h3>
@@ -28,11 +26,12 @@ export default function BlogList() {
 
                 </div>
                 <div className="col-4">
-                    <img src={micImg} alt="" />
+                    <img src={micImg} alt="" className='micimg' />
                 </div>
-
+                <div className="row mt-5 justify-content-center">
+                    <div className="col-10"><hr /></div>
+                </div>
                 <div className="row mt-5 moreblog">
-                    <hr />
                     {
                         [1, 2, 3].map(item => (
                             <div className="col-4 mt-5">
@@ -58,41 +57,12 @@ export default function BlogList() {
                 </div>
                 <RxDoubleArrowDown className='arrow' />
 
-
-                {/* <div className="col-12 text-center">
-                    <div className="row">
-                        <div className="col-3 mt-3 df"><hr /></div>
-                        <div className="col-6"><h2>  Trending around the world  </h2></div>
-                        <div className="col-3 mt-3 df"><hr /></div>
-                    </div>
-
-                </div>
-                {
-                    [1, 2, 3, 4, 5, 6, 7, 8].map(item => (
-                        <div className="col-6 justify-content-center list-sec mt-5">
-                            <div className="row">
-                                <div className="col-5">
-                                    <img src={blogimg} alt="" />
-                                </div>
-                                <div className="col-4">
-                                    <h2>The most haunted places in USA</h2>
-                                    <p>
-                                        All “Thriller”, no filter with our creepy countdownof the greatest ever Halloween song
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                } */}
-
             </div>
-            <div className="row suggested-sec  ">
-                <div className="highligt"></div>
-                <div className="col-3">
-                    <h2 className=''>SUGGESTED <br /> EVENTS FROM <br /><span className='spansomething'>  SOMETHINGTODO <br /> NEARBY </span></h2>
-                </div>
+            <div className="row circle-img  ">
 
-                {
+
+
+                {/* {
                     [1, 2, 3, 4, 5, 6, 7].map(item => (
                         <div className="col-3">
                             <div className="date-cards">
@@ -100,17 +70,137 @@ export default function BlogList() {
                             </div>
                         </div>
                     ))
-                }
+                } */}
 
             </div>
-            <div className="row justify-content-center">
-                <div className="col-12">
-                    <p className='text-center'>
-                        <CustomButton text='VIEW MORE' />
-                    </p>
+            <div className="row justify-content-center suggested-sec">
+                <div className="col-12 mt-5">
+                    <div className="row first-row">
+                        <div className="highligt"></div>
+                        <div className="col-3">
+                            <h2 className=''>SUGGESTED  EVENTS FROM <span className='spansomething'>  SOMETHINGTODO  NEARBY </span></h2>
+                        </div>
+                        {/* {
+                     [1, 2, 3, 4, 5, 6, 7].map(item => ( */}
+                        <div className="col-3 ">
+                            <div className="date-cards">
+                                <DateCard />
+                            </div>
+                        </div>
+                        <div className="col-3 ">
+                            <div className="date-cards">
+                                <DateCard />
+                            </div>
+                        </div>
+                        <div className="col-3 ">
+                            <div className="date-cards">
+                                <DateCard />
+                            </div>
+                        </div>
+
+                        {/* ))
+                } */}
+                    </div>
+
+                </div>
+                <div className="col-10">
+                    <div className="row ">
+                        <div className="col-4 ">
+                            <div className="date-cards">
+                                <DateCard />
+                            </div>
+                        </div>
+                        <div className="col-4 ">
+                            <div className="date-cards">
+                                <DateCard />
+                            </div>
+                        </div>
+                        <div className="col-4 ">
+                            <div className="date-cards">
+                                <DateCard />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center button-style">
+                    <div className="col-12">
+                        <p className='text-center'>
+                            <CustomButton text='VIEW MORE' />
+                        </p>
+                    </div>
                 </div>
             </div>
+            <div className="row mt-5 justify-content-center">
+                <div className="col-9"><hr /></div>
+            </div>
+            <div className="row  justify-content-center recent-blog">
+                <div className="recentBlogHighlight"></div>
+                <div className="col-4">
+                    <div className="row ">
+                        <div className="col-12 mt-4">
+                            <h2>SOMETHING <span className='span-color'>TO</span> DO</h2>
+                        </div>
+                        <div className="recent-blog-img mt-3">
+                            <div className="row">
+                                <div className="col-12">
+                                    <h3>BLOG NAME</h3>
+
+                                </div>
+                                <div className="col-11">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Lorem ipsum dolor sit ame</p>
+
+                                </div>
+                                <div className="row mb-3">
+                                    <div className="col-11">
+                                        <span className='span2'>Friday, Oct 2020</span>
+                                    </div>
+                                    <div className="col-1">
+                                        <img src={arrowForwardIcon} className="arrow-forward" alt="" />
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-1 text-center">
+                    <div class="vr vr-blurry " ></div>
+                </div>
+                <div className="col-4 second-sec">
+                    <div className="row justify-content-center ">
+                        <div className="col-12">
+                            <h2>RESENT POSTS</h2>
+                        </div>
+                        <div className="col-12 mt-5 text-center">
+                            <div className="row">
+                                <div className="col-8">
+                                    <img src={recentpost1} alt="" />
+
+                                </div>
+                                <div className="col-10 mt-2">
+                                    <h3>BLOG NAME</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 mt-5 text-center">
+                            <div className="row">
+                                <div className="col-8">
+                                    <img src={recentpost1} alt="" />
+
+                                </div>
+                                <div className="col-10 mt-2">
+                                    <h3>BLOG NAME</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
             <div className='similar-vanue-style'>
+                <div className="similarVanueHighlight"></div>
                 <SimilarVanues />
             </div>
         </div >
