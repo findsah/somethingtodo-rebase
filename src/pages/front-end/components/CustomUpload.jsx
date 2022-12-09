7a716ee1eb5d963fc33218701f8462f1cfd1ee04
Arman Ali <toplinegeeks@gmail.com>
2022-12-09 14:43:48 +0500

    custom input upload for single or multi images

---
+++ src/pages/front-end/components/CustomUpload.jsx
@@ -0,0 +1,43 @@
+/* 
+author: arman ali
+github: https://github.com/Arman-Arzoo
+whatsapp: +923430048341
+*/
+import React from 'react'
+import Dropzone from 'react-dropzone'
+
+const CustomUpload = ({ setFiles, setPreview }) => {
+
+    // a funtion to setfiles and setpreview obj
+    const FilesUpload = (acceptedFiles) => {
+
+        if (acceptedFiles) {
+            const fileArray = Array?.from(acceptedFiles)?.map(file => URL?.createObjectURL(file))
+            setPreview((prevImages) => prevImages?.concat(fileArray))
+            setFiles((prevImages) => prevImages?.concat(acceptedFiles))
+            // clean up url createObject
+            Array?.from(acceptedFiles)?.map((file) => {
+                URL?.revokeObjectURL(file)
+            })
+        }
+    }
+    return (
+        <Dropzone onDrop={acceptedFiles => { FilesUpload(acceptedFiles) }}>
+            {({ getRootProps, getInputProps }) => (
+                <section>
+                    <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        {/* a custom style */}
+                        <div className='add_image'  >
+                            <div className='icon_cirlce'>
+                                <i class="fa fa-plus" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            )}
+        </Dropzone>
+    )
+}
+
+export default CustomUpload
