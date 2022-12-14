305d6c9d32b48b05eb56b99814aa35ac49a1d223
shahidminhasgill <shahidminhas313@gmail.com>
2022-12-14 18:21:54 +0500

    login sign up complete

---
+++ src/pages/front-end/user-registeration/components/LoginFinishModal.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Modal, Toast, ToastContainer } from 'react-bootstrap';
+import { IoMdClose } from "react-icons/io";
+import done from "../../../../assets/icons/done_outline.svg";
+
+export default function LoginFinishModal({ modalShow, setModalShow }) {
+    return (
+        <div size={28}>
+            <Modal
+                show={modalShow} onHide={() => {
+                    setModalShow(false)
+                }}
+
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+
+                <Modal.Body>
+                    <div className="row d-flex justify-content-center">
+                        <div className="col-12 d-flex- justify-content-end">
+                            <IoMdClose
+                                onClick={() => setModalShow(false)}
+                                size={28}
+                                className="icon-color"
+                                style={{ cursor: "pointer" }}
+                            />
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-lg-9 col-md-6 col-sm-6">
+                            <div class="card-title text-center">
+                                <h2 class=" login-main-heading">
+                                    You have successfully Sign In
+                                    Thank you
+                                </h2>
+                                <img src={done} alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </div>
+    )
+}
---
+++ src/pages/front-end/user-registeration/components/LogoutModal.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Button, Modal, Toast, ToastContainer } from 'react-bootstrap';
+import { IoMdClose } from "react-icons/io";
+import done from "../../../../assets/icons/done_outline.svg";
+import logo from "../../../../assets/logo.png";
+
+export default function LogoutModal({ modalShow, setModalShow }) {
+    return (
+        <div>
+            <Modal
+                show={modalShow} onHide={() => {
+                    setModalShow(false)
+                }}
+
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+
+                <Modal.Body>
+                    <div className="row d-flex justify-content-center">
+                        <div className="col-lg-6 col-11">
+                            <img className="img-fluid" src={logo} alt="logo" />
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-lg-9 col-md-6 col-sm-6">
+                            <div class="card-title text-center">
+                                <h3 class=" login-main-heading" style={{ marginTop: '3rem' }}>
+                                    Do you want to log out ?
+                                </h3>
+                                <Button className='btn-main' style={{ marginTop: '4rem' }}>Yes</Button>
+                                <Button variant='outlined' style={{ marginTop: '1rem' }} className='outlined-btn'>No</Button>
+                            </div>
+
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </div>
+    )
+}
